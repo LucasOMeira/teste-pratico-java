7ea0fd5e9126d6d7985675b302b214bf6ba22f97
Lucas Meira <92492994+LucasOMeira@users.noreply.github.com>
2022-04-06 20:51:39 -0300

    Adicionando comentários

--- testepraticoprojectdata/src/testepraticoprojectdata/Pessoa.java
+++ testepraticoprojectdata/src/testepraticoprojectdata/Pessoa.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 public class Pessoa {
     private String nome;
     private LocalDate dataNascimento;
-
+    
+    //criando pessoas e seus atributos
     public Pessoa(String nome, LocalDate dataNascimento) {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
     }
-
+    
+    //getters e setters
     public String getNome() {
         return nome;
     }
@@ -17,7 +19,8 @@ public class Pessoa {
     public LocalDate getDataNascimento() {
         return dataNascimento;
     }
-
+    
+    //polimorfismo
     public String toString() {
         return nome + ", " + dataNascimento.getDayOfMonth() + "/" + dataNascimento.getMonthValue() + "/" + dataNascimento.getYear();
     }
