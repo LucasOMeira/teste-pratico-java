378fc2e2b37e23916101add7c97c2efdd8d6ad03
Lucas Meira <92492994+LucasOMeira@users.noreply.github.com>
2022-04-06 20:54:05 -0300

    Adicionando Comentários

--- testepraticoprojectdata/src/testepraticoprojectdata/Funcionario.java
+++ testepraticoprojectdata/src/testepraticoprojectdata/Funcionario.java
@@ -4,16 +4,19 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.Locale;
 
+//criando classe funcionario extendendo a classe pessoa
 public class Funcionario extends Pessoa {
     private String funcao;
     private BigDecimal salario;
-
+    
+    //definindo objeto funcionário e seus atributos
     public Funcionario(String nome, LocalDate dataNascimento, String funcao, BigDecimal salario) {
         super(nome, dataNascimento);
         this.funcao = funcao;
         this.salario = salario;
     }
 
+    //getters e setters
     public String getFuncao() {
         return funcao;
     }
@@ -29,16 +32,19 @@ public class Funcionario extends Pessoa {
     public void setSalario(BigDecimal salario) {
         this.salario = salario;
     }
-
+    
+    //formatando saída do salário
     public String getSalarioFormatado() {
         return NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(salario);
     }
 
+    //formatando saída de informação do funcionário
     @Override
     public String toString() {
         return super.toString() + ", Função: " + funcao + ", Salário: " + getSalarioFormatado();
     }
-
+    
+    //criando método de aumento de salário
     public void recebeAumento(BigDecimal aumento) {
         this.salario = this.salario.multiply(aumento);
     }
